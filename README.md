ab1fe330156da58c710075995b7ef5d648f1afa3
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-04 15:19:18 +0300

    ran first working simualtion

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Gambling
 
 Monte Carlo simulations to gauge which player maximizes winnings at guessing a Die's outcome
+
+## setup
+
+* A biased and unbiased die are created based on the probabilities of any sides showing face up.
+* Two players are created; one guessing the die value randomly and another using passed experience  of most occuring side to guess
+* A simulation is run n times to see overall correct guesses after n iterations
+
+![plot of simulation results](figure_1.png)
+
--- run.py
+++ run.py
@@ -1,10 +1,56 @@
+import matplotlib.pyplot as plt
+
 from src.Die import Die, BiasedDie
 from src.Player import GuessingPlayer, SmartPlayer
 
-def winnings_with_biased_die(n_iterations):
+def plot_winnings(n_iterations):
 
-    # setup die
-    die = BiasedDie(3)
+    # setup parameters
+    biased_die = BiasedDie(2)
+    unbiased_die = Die()
     smart_player = SmartPlayer()
+    smart_player_cumulative_win = []
     guessing_player = GuessingPlayer()
+    guessing_player_cumulative_win = []
 
+    # run simulation biased die
+    for _ in range(n_iterations):
+        smart_player.take_a_guess(biased_die)
+        smart_player_cumulative_win.append(smart_player.current_winnings)
+        guessing_player.take_a_guess(biased_die)
+        guessing_player_cumulative_win.append(guessing_player.current_winnings)
+        
+    fig, axes = plt.subplots(nrows=2)
+    
+
+    axes[0].plot(range(n_iterations), smart_player_cumulative_win, label="Smart player")
+    axes[0].plot(range(n_iterations), guessing_player_cumulative_win, label="Guessing player")
+    axes[0].set_title("Using a biased die")
+    axes[0].set_xlabel("number of iterations")
+    axes[0].set_ylabel("number of correct guesses")
+    axes[0].legend(loc="best")
+
+    # run simulation unbiased die
+    smart_player_cumulative_win = []
+    guessing_player_cumulative_win = []
+    
+    for _ in range(n_iterations):
+        smart_player.take_a_guess(unbiased_die)
+        smart_player_cumulative_win.append(smart_player.current_winnings)
+        guessing_player.take_a_guess(unbiased_die)
+        guessing_player_cumulative_win.append(guessing_player.current_winnings)
+        
+    axes[1].plot(range(n_iterations), smart_player_cumulative_win, label="Smart player")
+    axes[1].plot(range(n_iterations), guessing_player_cumulative_win, label="Guessing player")
+    axes[1].set_title("Using an unbiased die")
+    axes[1].set_xlabel("number of iterations")
+    axes[1].set_ylabel("number of correct guesses")
+    axes[1].legend(loc="best")
+
+
+    plt.show()
+
+
+
+
+plot_winnings(1000)
